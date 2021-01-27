402e25dfe29901744a6ef6f56e1bfc4afe226315
moodledev <moodledev@gmail.com>
2021-01-27 08:28:42 -0600

    DGNR-222 copy topics format

---
+++ lang/en/format_designer.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component Designer course format.
+ *
+ * @package   format_designer
+ * @copyright 2021 bdecent gmbh <https://bdecent.de>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['addsections'] = 'Add designer sections';
+$string['currentsection'] = 'This designer section';
+$string['editsection'] = 'Edit designer section';
+$string['editsectionname'] = 'Edit designer section name';
+$string['deletesection'] = 'Delete designer section';
+$string['newsectionname'] = 'New name for designer section {$a}';
+$string['sectionname'] = 'Designer section';
+$string['pluginname'] = 'Designer format';
+$string['section0name'] = 'General';
+$string['page-course-view-designer'] = 'Any course main page in Designer format';
+$string['page-course-view-designer-x'] = 'Any course page in Designer format';
+$string['hidefromothers'] = 'Hide designer section';
+$string['showfromothers'] = 'Show designer section';
+$string['privacy:metadata'] = 'The Designer format plugin does not store any personal data.';
