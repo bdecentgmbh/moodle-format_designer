08f522102837465f5e66edfd2038e44b360c9bce
bdecent <hello@bdecent.de>
2021-12-14 10:43:49 +0100

    Update version.php

--- version.php
+++ version.php
@@ -27,3 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2021120600;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2020110300;        // Requires this Moodle version.
 $plugin->component = 'format_designer';    // Full name of the plugin (used for diagnostics).
+$plugin->release = 'Version 1.0';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->supported = [40, 40];
