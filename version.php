00e6217b73bfe126ace116f8bf1006404982237f
Prasanna LMSACE <prasanna_t@lmsace.com>
2022-03-07 12:58:13 +0530

    JS Stale files updated

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022020303;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2022030700;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2020061500;        // Requires this Moodle version.
 $plugin->component = 'format_designer';  // Full name of the plugin (used for diagnostics).
