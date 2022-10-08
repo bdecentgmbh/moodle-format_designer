1de33e15643003f83540980081505e6d3f58b8ee
prasanna-lmsace <57126778+prasanna-lmsace@users.noreply.github.com>
2022-10-08 11:53:45 +0530

    Update the release.

--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2022100800;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2020061500;        // Requires this Moodle version.
 $plugin->component = 'format_designer';  // Full name of the plugin (used for diagnostics).
-$plugin->release = 'Version 1.3';
+$plugin->release = 'Version 1.4';
 $plugin->supported = [39, 311];
 $plugin->maturity  = MATURITY_STABLE;
