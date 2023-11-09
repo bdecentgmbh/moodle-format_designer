f307654e53e4e5943515988b726b97eb58f3fcb9
Raja Lmsace <raja.lmsace@gmail.com>
2023-11-09 19:39:08 +0530

    Video time activity section icon issue fixed. Version Updated.

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023110400;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2023110900;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2020061500;        // Requires this Moodle version.
 $plugin->component = 'format_designer';  // Full name of the plugin (used for diagnostics).
 $plugin->release = 'Version 1.5';
-$plugin->supported = [400, 401];
+$plugin->supported = [400, 403];
 $plugin->maturity  = MATURITY_STABLE;
